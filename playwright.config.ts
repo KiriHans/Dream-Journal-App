feba3ff8586300dbd296c0d07a1beeafcc3e2a62
KiriHans <github.mxuxw@slmail.me>
2023-11-29 22:30:09 -0500

    changed playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: "npm run dev  -- --mode='test'",
+      command: 'npm run dev',
       port: 5173,
       timeout: 120 * 1000,
       reuseExistingServer: !process.env.CI,
